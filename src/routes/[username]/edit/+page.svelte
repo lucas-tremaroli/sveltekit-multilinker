9533a195fae13029e775ef31cacbac4fe4ecad1a
LucasLuigiT <lucas.tremaroli@lego.com>
2024-07-14 18:54:58 +0200

    feat: sortable drag and drop

--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { page } from "$app/stores";
     import UserLink from "$lib/components/UserLink.svelte";
+    import SortableList from "$lib/components/SortableList.svelte";
     import { db, userData, user } from "$lib/firebase";
     import {
         arrayRemove,
@@ -48,6 +49,12 @@
         showForm = false;
     }
 
+    function sortList(e: CustomEvent) {
+        const newList = e.detail;
+        const userRef = doc(db, "users", $user!.uid);
+        setDoc(userRef, { links: newList }, { merge: true });
+    }
+
     async function deleteLink(item: any) {
         const userRef = doc(db, "users", $user!.uid);
         await updateDoc(userRef, {
@@ -66,6 +73,21 @@
         <h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">
             Edit your profile
         </h1>
+        <SortableList
+            list={$userData?.links}
+            on:sort={sortList}
+            let:item
+            let:index
+        >
+            <div class="group relative">
+                <UserLink {...item} />
+                <button
+                    on:click={() => deleteLink(item)}
+                    class="btn btn-xs btn-error invisible group-hover:visible transition-all absolute -right-6 bottom-10"
+                    >Delete</button
+                >
+            </div>
+        </SortableList>
         {#if showForm}
             <form
                 on:submit|preventDefault={addLink}
