2f1f332d691887f29e23ce35905b63e54aeff17f
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2025-03-28 22:57:08 +0100

    feat: hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1 class="text-green-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Hello there</h1>
+            <p class="py-6">
+                This is a personal project to learn SvelteKit, leveraging
+                TailwindCSS with DaisyUI.
+            </p>
+            <button
+                class="btn btn-primary"
+                on:click={() => (window.location.href = "/login")}
+                >Get Started</button
+            >
+        </div>
+    </div>
+</div>
